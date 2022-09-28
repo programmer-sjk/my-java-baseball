0980ba1cdb0eaf0bac14bc514eaba5c4aded8d5e
sjk5766 <wjdrnr6662@gmail.com>
2022-09-28 15:07:27 +0900

    학습테스트 - set 테스트 코드 추가

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,39 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("집합의 크기를 구할 수 있다")
+    @Test
+    void 크기를_구할수있다() {
+        assertThat(numbers.size()).isEqualTo(4);
+    }
+
+    @DisplayName("집합에 포함여부를 알 수 있다")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void contains(String input, String expect) {
+        Boolean result = Boolean.parseBoolean(expect);
+        assertThat(numbers.contains(Integer.parseInt(input))).isEqualTo(result);
+    }
+}

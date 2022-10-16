887f19b708fa4b0dd8578be42204b85df7e5cb50
sjk5766 <wjdrnr6662@gmail.com>
2022-10-16 19:41:18 +0900

    refactor(enum): 볼, 스트라이크, 낫싱을 enum 타입으로 추가

---
+++ src/main/java/baseball/constant/ResultType.java
@@ -0,0 +1,18 @@
+package baseball.constant;
+
+public enum ResultType {
+    STRIKE("스트라이크"),
+    BALL("볼"),
+    NOTHING("낫싱");
+
+    private final String result;
+
+    ResultType(String result) {
+        this.result = result;
+    }
+
+    @Override
+    public String toString() {
+        return this.result;
+    }
+}

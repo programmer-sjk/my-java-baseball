e70c0e62df5e5159c36f93e8a46db52458d17b7f
sjk5766 <wjdrnr6662@gmail.com>
2022-10-19 22:08:00 +0900

    test: RefereeTest의 Display 설명 수정

--- src/test/java/baseball/model/RefereeTest.java
+++ src/test/java/baseball/model/RefereeTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RefereeTest {
-    @DisplayName("컴퓨터는 입력값에 대해 strike 개수를 구할 수 있다")
+    @DisplayName("심판은 컴퓨터의 숫자와 입력값을 받아 strike 개수를 구할 수 있다")
     @ParameterizedTest
     @CsvSource(value = {"123:1", "185:1", "238:2", "138:3", "418:1", "538:2"}, delimiter = ':')
     void 스트라이크_개수를_구한다(String input, int expect) {
@@ -19,7 +19,7 @@ public class RefereeTest {
         assertThat(referee.getStrikeCount()).isEqualTo(expect);
     }
 
-    @DisplayName("컴퓨터는 입력값에 대해 ball 개수를 구할 수 있다")
+    @DisplayName("심판은 컴퓨터의 숫자와 입력값을 받아  ball 개수를 구할 수 있다")
     @ParameterizedTest
     @CsvSource(value = {"123:0", "185:1", "163:1", "654:2", "615:2", "564:3"}, delimiter = ':')
     void 볼_개수를_구한다(String input, int expect) {
